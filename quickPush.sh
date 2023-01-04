4e6a5662323ff5b5fb4e756e786f6fd13a689dc9
Thodoris Velmachos <t.velmachos@ngcloudops.com>
2023-01-04 21:28:27 +0200

    quick commit - 01/04/2023-21:28:27

--- quickPush.sh
+++ quickPush.sh
@@ -1,1 +1,5 @@
+git config --global user.name "Thodoris Velmachos"
+git config --global user.email "t.velmachos@ngcloudops.com"
+
+
 git add -A && git commit -m "quick commit - `date +%m/%d/%Y-%H:%M:%S`" && git push origin
